67a7a9aa0c57eb5f79b9e59b8c4a7d767da8746a
Daniel Severo <daniel.severo@sap.com>
2020-09-23 16:33:55 -0300

    Added redis addon Created service instance of new redis service binding new redis service instance with numbers-service serverless Log the REDIS port on console of lambda Started the readme.md of exercise 05

---
+++ exercises/05/number-generator-base-project/js/numberGenerator.js
@@ -0,0 +1,94 @@
+// The lottery expects the player to pick 5 number + a so called powerball which gets generated sperately.
+// The generatedNumbers array represents the first 5 numbers.
+var client_id=atob('OWJiYmU0OGQtMTQ2ZC00NGUxLThjZDMtNTVhZGM0NmRiZjI0');
+var client_secret=atob('MkdQZkFFQ3QtcFJIZ1ppalZoSUltemhWOHI=');
+var encodedData = btoa(client_id + ':' + client_secret);
+
+var urlToken = '/oauth2/token';
+var urlNumberService = 'https://number-generator-service.kyma.local';
+var urlNumbersService = 'https://numbers-generator-service.kyma.local';
+
+function fetchToken(){
+    fetch(urlToken, {
+        method: 'POST',
+        body: 'grant_type=client_credentials&scope=read',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Authorization',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE' ,
+            'client_id': client_id,
+            'client_secret': client_secret,
+            'Authorization': 'Basic ' + encodedData
+        }
+    }).then(function (resp) {
+        return resp.json();
+
+    }).then(function (data) {
+        console.log('token', data.access_token);
+
+        generate(data.access_token);
+        generateNumber(data.access_token);
+
+        return data.access_token;
+
+    }).catch(function (err) {
+        console.log('something went wrong', err);
+    });
+}
+
+
+// This method generates a random number between 1 0 and 69.
+// returns a random number.
+function generateNumber(bearerToken) {
+    fetch(urlNumberService, {
+        headers: {
+            'Authorization': 'bearer ' + bearerToken,
+        }})
+        .then(function (response){
+            return response.json();
+        }).then(function (data){
+        console.log(data);
+        document.getElementById('pNumberLabel').textContent = 'Powerball: ' + data;
+    }).catch(function (err){
+        console.warn('Something went wrong.', err);
+    })
+}
+
+// This method iterates over the generatedNumbers array and fills it with randomly generated numbers through the generateNumber() method.
+// returns an array of 5 randomly generated numbers.
+function generate(bearerToken) {
+    fetch(urlNumbersService, {
+        headers: {
+            'Authorization': 'bearer ' + bearerToken,
+        }})
+        .then(function (response){
+            return response.json();
+        }).then(function (data){
+        console.log(data);
+        document.getElementById('baseNumbersLabel').textContent = data;
+    }).catch(function (err){
+        console.warn('Something went wrong.', err);
+    })
+}
+
+// Calls the genrateNumber() and generate() method.
+// returns the first 5 numbers and the so called powerball.
+function giveWinningNumbers() {
+    fetchToken();
+}
+
+function giveWinningNumbersHistory(){
+    fetch(urlNumbersService, {
+        headers: {
+            'Authorization': 'bearer ' + bearerToken,
+        }})
+        .then(function (response){
+            return response.json();
+        }).then(function (data){
+        console.log(data);
+        document.getElementById('baseNumbersLabel').textContent = data;
+    }).catch(function (err){
+        console.warn('Something went wrong.', err);
+    })
+}
--- exercises/05/readme.md
+++ exercises/05/readme.md
@@ -1,3 +1,16 @@
 # Exercise 5 - Create extensions to get Additional Services
 
+## Introduction - Scenario
+In this exercise you will get introduced to the Serverless functionality of Kyma and how to expose them via OAuth2 secured API. 
+This exercise will continue to use our web application created in the previous exercise, it will be modified replacing the local functions to call the new Kyma Serverless Services exposed.
+The exercise 4, also generated a ready to use Docker image of the web application, which can led you to focus on the Kyma activities
+
+The goal is to create new Serverless functions and secure them via OAuth2 API using your *project: "Kyma"* installation.
+
+### You Will Learn How to: 
+
+- Create an Serverless service in the *project: "Kyma"*
+- Create an OAuth2 client credentials
+- Configure a Serverless service to be authenticated using OAuth2 
+
 ## Steps
